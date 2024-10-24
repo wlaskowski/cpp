f14bde23a4fff1b527fe795ad3ab16df41574332
wlaskowski <148315550+wlaskowski@users.noreply.github.com>
2024-10-25 00:26:47 +0200

    Update main.cpp

--- find_motifs/main.cpp
+++ find_motifs/main.cpp
@@ -20,11 +20,33 @@ public:
         std::cout << "Enter genome sequence: " << std::endl;
         std::cin >> genome_seq;
     }
+
+    void find_motifs() {
+        int motif_len = motif.length();
+        int genome_len = genome_seq.length();
+
+        for (int i = 0; i <= genome_len - motif_len; i++) {
+            std::string genome_substring = genome_seq.substr(i, motif_len);
+            int mismatches = 0;
+
+            for (int j=0; j <= motif_len; j++) {
+                if (motif[j] != genome_substring[j]) {
+                    mismatches++;
+                }
+            }
+            
+            if (mismatches <= edit_dist) {
+                std::cout << "Motif: " << motif << std::endl << "contains: " << mismatches << " mismatches";
+            }
+        }
+    }
+
 };
 
 int main(){
     DNA_data dna;
     dna.set_data();
+    dna.find_motifs();
 
     return 0;
 }
